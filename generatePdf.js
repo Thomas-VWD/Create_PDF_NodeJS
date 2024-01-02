7730f03cab5203957c07c5577fdb85c37180d5ec
Thomas-VWD <thomas.verwaerde@gmail.com>
2024-01-02 19:13:51 +0100

    Play with polygons

--- generatePdf.js
+++ generatePdf.js
@@ -13,7 +13,7 @@ doc
   .fillColor("#4169E1")
   .font("Fonts/Roboto/Roboto-Bold.ttf")
   .fontSize(25)
-  .text("2024 Asia import prevision ", 10, 100, { align: "center" });
+  .text("2024 Asia import prevision", 10, 100, { align: "center" });
 
 // Add an image, constrain it to a given size, and center it vertically and horizontally
 doc.image("assets/containerShip.jpg", 5, 200, {
@@ -21,10 +21,35 @@ doc.image("assets/containerShip.jpg", 5, 200, {
 });
 
 // Add another page
-doc.addPage().fontSize(25).text("Here is some vector graphics...", 100, 100);
+doc.addPage().fontSize(25).text("Play with polygons", 100, 100);
 
 // Draw a triangle
-doc.save().moveTo(100, 150).lineTo(100, 250).lineTo(200, 250).fill("#FF3300");
+//doc.save().moveTo(100, 150).lineTo(100, 250).lineTo(200, 250).fill("#FF3300");
+doc.save().moveTo(50, 150).lineTo(50, 250).lineTo(200, 250).fill("#4169E1");
+
+// Dessiner un carré
+const xPosition = 250;
+const yPosition = 150;
+const sideLength = 100;
+
+doc.rect(xPosition, yPosition, sideLength, sideLength).fill("#FF3300");
+
+// Dessiner un rectangle
+const xRectPosition = 400;
+const yRectPosition = 150;
+const rectWidth = 200;
+const rectSideLength = 100;
+
+doc
+  .rect(xRectPosition, yRectPosition, rectWidth, rectSideLength)
+  .fill("#4169E1");
+
+// Line Cap
+doc.lineWidth(50);
+// line cap settings
+doc.lineCap("butt").moveTo(50, 350).lineTo(100, 350).stroke();
+doc.lineCap("round").moveTo(150, 350).lineTo(200, 350).stroke();
+doc.lineCap("square").moveTo(250, 350).circle(275, 350, 15).stroke();
 
 // Apply some transforms and render an SVG path with the 'even-odd' fill rule
 doc
